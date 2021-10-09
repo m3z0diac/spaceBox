34afdb12af182aacdc44dae3ef2276c4eaca7cb0
hamza07-w <ansari17102002@gmail.com>
2021-10-09 03:00:25 +0200

    spaceBox

---
+++ administrator/dashboard.php
@@ -0,0 +1,46 @@
+<?php
+
+if (count($_COOKIE)>0 && isset($_COOKIE['user'])) {
+  
+  if ($_COOKIE['user'] == "admin") {
+    http_response_code(200);
+  }
+  elseif ($_COOKIE['user'] == "hamza") {
+    header('Location: index.php');
+  }
+}
+
+else {
+    
+    http_response_code(403);
+    die("<center><h1 style='color:blue';padding-top:25%;> Forbidden 403<br>you are not allowd to see this page<br>:(</h1></center>");
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>space box - admin login</title>
+	<link rel="stylesheet" type="text/css" href="../css/home.css">
+</head>
+<body>
+    <ul>
+        <li style="float: left;"><a href="#">login</a></li>
+        <li style="float: left;"><a href="logout.php">logout</a></li>
+        <li><a href="../pics.php">pictures</a></li>
+        <li><a href="../posts.php">posts</a></li>
+        <li><a href="../index.php">Home</a></li>
+    </ul>
+    <div class="home">
+    	<h2>welcom to admin's dashboaed</h2>
+        <h3>Note: all your uploaded posts going to /poasts dir</h3>
+		<form method="post" action="../posts.php">
+            <input class="anyinput" type="text" name="article-title" placeholder="article title" required><br>
+			<textarea style="width: 40%" name="article" placeholder="your article here" required></textarea><br>
+			<input type="submit" name="submit" value="post" required>
+		</form>
+    </div>
+</body>
+</html>
---
+++ administrator/readmeifyoucan/config.php
@@ -0,0 +1,3 @@
+<?php
+	$flag = "hkg{congrats}"
+?>
