7ebf78c1dd82bc9d05de2ec05c58f8d53700a22b
Hamza Elansari <72104163+hamza07-w@users.noreply.github.com>
2021-10-09 13:41:09 +0200

    Update cookies.php

--- administrator/cookies.php
+++ administrator/cookies.php
@@ -2,7 +2,6 @@
 if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username']) && isset($_POST['password'])){
   $user = htmlspecialchars($_POST['username']);
   $pass = htmlspecialchars($_POST['password']);
-}
   if ($user == "admin" && $pass == "randompass") {
     setcookie("user", "admin", time()+3600, "/");
     header("Location: dashboard.php");
@@ -11,7 +10,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username']) && isset($
     setcookie("user", "hamza");
     header("Location: ../index.php");
   }
-
+}
 else {
     echo "forbiden";
 }
