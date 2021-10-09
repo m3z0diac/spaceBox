ff2d198227d3279c183d50d1680ca40b3747800a
hamza07-w <ansari17102002@gmail.com>
2021-10-09 23:56:56 +0200

    gif added

--- administrator/login.php
+++ administrator/login.php
@@ -1,21 +1,15 @@
 <?php
 
-if (count($_COOKIE)>0 && isset($_COOKIE['user'])) {
+if (count($_COOKIE)>0 && isset($_COOKIE['admin'])) {
   
   if ($_COOKIE['user'] == base64_encode("admin")) {
-    http_response_code(200);
+    header('Location: dashboard.php');
   }
   elseif ($_COOKIE['user'] == base64_encode("hamza")) {
     header('Location: ../index.php');
   }
 }
 
-else {
-    
-    http_response_code(403);
-    die("<center><h1 style='color:blue';padding-top:25%;> Forbidden 403<br>you are not allowd to see this page<br>:(</h1></center>");
-    exit;
-}
 
 ?>
 
@@ -28,19 +22,16 @@ else {
 <body>
     <ul>
         <li style="float: left;"><a href="#">login</a></li>
-        <li style="float: left;"><a href="logout.php">logout</a></li>
         <li><a href="../pics.php">pictures</a></li>
         <li><a href="../posts.php">posts</a></li>
         <li><a href="../index.php">Home</a></li>
     </ul>
     <div class="home">
-      <h2>welcom to admin's dashboaed</h2>
-        <center><img style="width: 300px;height: 200px;" src="../img/noice.gif"></center>
-        <h3>Note: all your uploaded posts going to /posts dir</h3>
-    <form method="post" action="../posts.php">
-            <input class="anyinput" type="text" name="article-title" placeholder="article title" required><br>
-      <textarea style="width: 40%" name="article" placeholder="your article here" required></textarea><br>
-      <input type="submit" name="submit" value="post" required>
+      <h2>log into admin control panel</h2>
+    <form method="post" action="cookies.php">
+      <input class="anyinput" type="text" name="username" placeholder="username" required><br>
+      <input class="anyinput" type="password" name="password" placeholder="password" required><br>
+      <input type="submit" name="submit" value="login" required>
     </form>
     </div>
 </body>
